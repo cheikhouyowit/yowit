05b8297c8b4de5372d9bf3c78045e02438306b15
Cheikh Ahmed Tidiane Diop <78166974+cheikhatdiop00@users.noreply.github.com>
2022-08-02 15:35:15 +0000

    devdingo

--- yowit_vibe/models/index.py
+++ yowit_vibe/models/index.py
@@ -3,5 +3,9 @@ from odoo import fields, models, api
 class Index(models.Model):
     _name='index.index'
     _description= 'ceci est un teste de devdingo'
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
     
     

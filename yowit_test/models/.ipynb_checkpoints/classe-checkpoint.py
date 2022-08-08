dc2a6537c72d7bbafe28d3d3fc46cd1d488312fe
vibedarkness <you@gmail.com>
2022-08-08 13:25:09 +0000

    testdingo

--- yowit_test/models/.ipynb_checkpoints/classe-checkpoint.py
+++ yowit_test/models/.ipynb_checkpoints/classe-checkpoint.py
@@ -5,7 +5,7 @@ class Classe(models.Model):
     _name= "classe.yowit"
     nom_classe=fields.Char(string="nom")
     code=fields.Char()
-    apprenant_id=fields.One2Many(comodel_name='apprenant.yowit', inverse_name='classe_id')
+    apprenant_id=fields.One2many(comodel_name='apprenant.yowit', inverse_name='classe_id')
     professeur_id=fields.Many2many(comodel_name='professeur.yowit', relation='class_prof', column1="nom", column2="prenom"  )
     matiere_id=fields.Many2many(comodel_name='matiere.yowit', relation='class_mat', column1="nom_classe", column2="nom"  )
 

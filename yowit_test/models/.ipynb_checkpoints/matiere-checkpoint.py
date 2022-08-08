9eaac28910f7d5f73601e392c63ce5c38c3215af
vibedarkness <you@gmail.com>
2022-08-08 14:03:08 +0000

    testdingo

--- yowit_test/models/.ipynb_checkpoints/matiere-checkpoint.py
+++ yowit_test/models/.ipynb_checkpoints/matiere-checkpoint.py
@@ -5,7 +5,7 @@ class Matiere(models.Model):
     _name= "matiere.yowit"
     nom=fields.Char("nom")
     code=fields.Char()
-    departement_id=fields.Many2one(comodel_name='departement.yowit')
-    professeur_id=fields.Many2many(comodel_name='professeur.yowit', relation='mat_prof', column1="nom", column2="prenom"  )
+    departement_id=fields.Many2one('departement.yowit')
+    professeur_id=fields.Many2many('professeur.yowit', relation='mat_prof', column1="nom", column2="prenom"  )
 
     
--- yowit_test/models/classe.py
+++ yowit_test/models/classe.py
@@ -6,7 +6,7 @@ class Classe(models.Model):
     nom_classe=fields.Char(string="nom")
     code=fields.Char()
     apprenant_id=fields.One2many(comodel_name='apprenant.yowit', inverse_name='classe_id')
-    professeur_id=fields.Many2many(comodel_name='professeur.yowit', relation='class_prof', column1="nom", column2="prenom"  )
-    matiere_id=fields.Many2many(comodel_name='matiere.yowit', relation='class_mat', column1="nom_classe", column2="nom"  )
+    professeur_id=fields.Many2many('professeur.yowit', relation='class_prof', column1="nom", column2="prenom"  )
+    matiere_id=fields.Many2many('matiere.yowit', relation='class_mat', column1="nom_classe", column2="nom"  )
 
     
